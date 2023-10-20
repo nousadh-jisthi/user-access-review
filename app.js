c7f541baee5091c43bc138b1938c742656e6200a
nousadh-jisthi <53829587+nousadh-jisthi@users.noreply.github.com>
2023-10-20 17:42:40 -0400

    Parsing user information

--- app.js
+++ app.js
@@ -22,10 +22,8 @@ client.bind(ldapOptions.bindDN, ldapOptions.bindCredentials, (err) => {
     
     var opts = {
         filter: '(objectClass=inetOrgPerson)',  //simple search
-        //  filter: '(&(uid=2)(sn=John))',// and search
-        //filter: '(|(uid=2)(sn=John)(cn=Smith))', // or search
         scope: 'sub',
-        attributes: []
+        attributes: ['sn', 'cn', 'uid', 'manager']
     };
 
     client.search('ou=users,ou=system', opts, function (err, res) {
@@ -36,7 +34,8 @@ client.bind(ldapOptions.bindDN, ldapOptions.bindCredentials, (err) => {
                 console.log('searchRequest: ', searchRequest.messageId);
               });
               res.on('searchEntry', (entry) => {
-                console.log('entry: ' + JSON.stringify(entry.pojo));
+                //console.log('entry: ' + JSON.stringify(entry.pojo));
+                parseUser(entry);
               });
               res.on('searchReference', (referral) => {
                 console.log('referral: ' + referral.uris.join());
@@ -46,21 +45,48 @@ client.bind(ldapOptions.bindDN, ldapOptions.bindCredentials, (err) => {
               });
               res.on('end', (result) => {
                 console.log('status: ' + result.status);
+
+                // Close the connection when you're done
+                client.unbind((unbindErr) => {
+                    if (unbindErr) {
+                    console.error('LDAP Unbind Error:', unbindErr);
+                    } else {
+                    console.log('LDAP Unbind Successful');
+                    }
+                });
               });
         }
     });
-
-    // Close the connection when you're done
-    /*client.unbind((unbindErr) => {
-      if (unbindErr) {
-        console.error('LDAP Unbind Error:', unbindErr);
-      } else {
-        console.log('LDAP Unbind Successful');
-      }
-    });*/
   }
 });
 
+// Function to parse information of user from entry
+function parseUser(entry) {
+    attributes = entry.pojo.attributes;
+    // For loop to go over attributes
+    var user = {}
+    user["dn"] = entry.pojo.objectName
+    attributes.forEach(attribute => {
+        user[attribute.type] = attribute.values[0]
+        /*if (attribute.type === 'cn') {
+            console.log('Name:', attribute.values[0]);
+            
+        }
+        else if (attribute.type === 'sn') {
+            console.log('Surname:', attribute.values[0]);
+            user.sn = attribute.values[0]
+        }
+        else if (attribute.type === 'uid') {
+            console.log('Username:', attribute.values[0]);
+            user.uid = attribute.values[0]
+        }
+        else if (attribute.type === 'manager') {
+            console.log('Manager DN:', attribute.values[0]);
+            user.manager = attribute.values[0]
+        }*/
+    });
+    console.log(JSON.stringify(user))
+}
 
 // Handle LDAP client events
 client.on('error', (err) => {
