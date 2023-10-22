fa0288d74983f610ce011e0b12cc7443365ed3b5
nousadh-jisthi <53829587+nousadh-jisthi@users.noreply.github.com>
2023-10-22 16:12:38 -0400

    Dynamic logging of sequelize based on env vars

--- config/config.js
+++ config/config.js
@@ -6,20 +6,23 @@ module.exports = {
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "logging": process.env.SEQUELIZE_LOGGING === 'true' ? console.log : false
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "logging": process.env.SEQUELIZE_LOGGING === 'true' ? console.log : false
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "logging": process.env.SEQUELIZE_LOGGING === 'true' ? console.log : false
   }
 }
